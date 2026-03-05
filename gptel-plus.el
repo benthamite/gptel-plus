67d7d27b34c9b48cdb2ab812023cd6d3a4ecc958
Pablo Stafforini <pablo@stafforini.com>
2026-03-05 11:57:55 -0300

    Bump version to 0.2.0

--- gptel-plus.el
+++ gptel-plus.el
@@ -4,7 +4,7 @@
 
 ;; Author: Pablo Stafforini
 ;; URL: https://github.com/benthamite/gptel-plus
-;; Version: 0.1
+;; Version: 0.2
 ;; Package-Requires: ((emacs "29.1") (gptel "0.7.1"))
 
 ;; This file is NOT part of GNU Emacs.
