96031411e91f6b14c55d68781d7f4667b5de2ad9
Pablo Stafforini <pablo@stafforini.com>
2026-03-05 10:25:24 -0300

    Bump copyright year to 2026 and add GPL-3 license file

--- gptel-plus.el
+++ gptel-plus.el
@@ -1,6 +1,6 @@
 ;;; gptel-plus.el --- Enhancements for gptel -*- lexical-binding: t -*-
 
-;; Copyright (C) 2025
+;; Copyright (C) 2025-2026 Pablo Stafforini
 
 ;; Author: Pablo Stafforini
 ;; URL: https://github.com/benthamite/gptel-plus
